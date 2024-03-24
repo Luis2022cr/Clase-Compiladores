e7f075da600081c5222154d89c781a736197a1a8
Luis2022cr <joseluiscrz129@gmail.com>
2024-03-23 21:55:39 -0600

    update Info Character DragonBall

--- Unidad1/Ejemplo1/src/dragon-ball/Index.ts
+++ Unidad1/Ejemplo1/src/dragon-ball/Index.ts
@@ -1,4 +1,5 @@
 export { CharacterCard } from './components/CharacterCard';
 
+export type { Character } from './interfaces/character';
 export type { SimplePersonajes } from './interfaces/Simple-Personajes';
 export type { PersonajesResponse } from './interfaces/personajes-response';
